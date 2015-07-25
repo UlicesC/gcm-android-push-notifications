a66c0ec262fe6b771d01cb2393b531ff2fe691ec
UlicesC <ulicescarlosmr@gmail.com>
2015-07-25 15:38:06 -0400

    gcmserver set on Serer

---
+++ gcmserver/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Database config variables
+ */
+//            'username' => 'admindb',
+//            'username' => 'postgres',
+//            'password' => 'sinap2709',
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "admin100990");
+define("DB_DATABASE", "gcm");
+
+/*
+ * Google Cloud Messaging API Key
+ */
+define("GOOGLE_API_KEY", "AIzaSyADzu44BB0oj8wqKbCXl5azZfkafDkRjJs"); // Place your Google API Key
+                          
+?>
