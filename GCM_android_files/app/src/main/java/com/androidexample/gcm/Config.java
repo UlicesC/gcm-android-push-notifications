52c858249e4ab3ab15c979c681452a969a4a18e5
UlicesC <ulicescarlosmr@gmail.com>
2015-07-25 11:41:09 -0400

    Add GCM_adroid_files folder and Readme.txe file

---
+++ GCM_android_files/app/src/main/java/com/androidexample/gcm/Config.java
@@ -0,0 +1,22 @@
+package com.androidexample.gcm;
+
+public interface Config {
+
+	
+	// CONSTANTS
+	static final String YOUR_SERVER_URL =  "http://192.168.0.12:8080/gcmserver/register.php";
+	// YOUR_SERVER_URL : Server url where you have placed your server files
+    // Google project id
+    static final String GOOGLE_SENDER_ID = "877060652743";  // Place here your Google project id
+    /**
+     * Tag used on log messages.
+     */
+    static final String TAG = "GCM Android Example";
+
+    static final String DISPLAY_MESSAGE_ACTION =
+            "com.androidexample.gcm.DISPLAY_MESSAGE";
+
+    static final String EXTRA_MESSAGE = "message";
+		
+	
+}
